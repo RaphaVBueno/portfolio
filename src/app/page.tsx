7d016e25b1361a36009079d46329345ef8a6d22a
Raphael V. Bueno <rafavbueno@gmail.com>
2025-01-31 00:04:28 -0300

    primeiro commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,79 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+'use client'
+
+import Image from 'next/image'
+import styles from './page.module.css'
 
 export default function Home() {
   return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.tsx</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
+    <div>
+      <header className={styles.header}>
+        <h1>R</h1>
+        <div className={styles.divNav}>
+          <nav>Sobre mim</nav>
+          <nav>Projetos</nav>
+          <nav>Hard skills</nav>
+          <nav>Soft Skills</nav>
         </div>
+      </header>
+      <main>
+        <section className={styles.section}>
+          <div>
+            <p>Olá visitante, eu sou</p>
+            <h1>Raphael V. Bueno</h1>
+            <h2>Desenvolvedor Web Fullstack</h2>
+            <button className={styles.buttonCv}>Download CV</button>
+          </div>
+          <div className={styles.avatar}>
+            <Image
+              src="/avatar.png"
+              alt="meu avatar"
+              width={600}
+              height={650}
+            />
+          </div>
+        </section>
+        <section className={styles.section2}>
+          <h2>Sobre Mim</h2>
+          <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+            Consequuntur, ad excepturi. Modi, corrupti possimus. Numquam sunt a
+            commodi adipisci laborum esse similique? Ex voluptas dolorum optio
+            similique magni tempore quidem. Lorem ipsum dolor sit amet
+            consectetur, adipisicing elit. Repellat sed libero facere assumenda
+            nemo ipsam asperiores iusto, obcaecati, quas tempora eos voluptatem
+            velit, quo eius ducimus nesciunt voluptas earum beatae. Lorem ipsum
+            dolor sit amet consectetur adipisicing elit. Iste quisquam eius qui
+            maxime facilis. Asperiores provident dolores vel tempora facere
+            assumenda dolor delectus at. Iste iusto deleniti aliquam non ullam?
+          </p>
+        </section>
+        <section className={styles.section2}>
+          <div className={styles.menu}>
+            <button className={styles.menuButton1}>Projetos</button>
+            <button className={styles.menuButton}>hard skills</button>
+            <button className={styles.menuButton}>soft skills</button>
+          </div>
+          <div className={styles.content}>
+            <p>
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fuga
+              reprehenderit veritatis repellat minima repellendus? Possimus eos
+              inventore consectetur, iste vitae tenetur placeat at voluptate cum
+              provident cupiditate? Pariatur, iste id. Lorem, ipsum dolor sit
+              amet consectetur adipisicing elit. Omnis quo sed distinctio non
+              aspernatur laborum eius, ea hic facere fugiat veritatis nisi!
+              Atque unde perspiciatis, nam dolorem nulla excepturi nihil? Lorem
+              ipsum dolor sit amet consectetur, adipisicing elit. Hic corrupti
+              libero, accusantium dolorum, est placeat ad eveniet mollitia
+              maiores corporis, tenetur quisquam natus deserunt iusto modi! Quo
+              necessitatibus recusandae modi. Lorem ipsum dolor sit amet
+              consectetur adipisicing elit. Unde earum rerum repellendus ratione
+              maiores illum! A, nam animi! Autem eius quidem asperiores quasi
+              tempora vel sunt nam exercitationem porro at?
+            </p>
+          </div>
+        </section>
       </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+      <footer></footer>
     </div>
-  );
+  )
 }
